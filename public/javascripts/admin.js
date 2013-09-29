872ddbff708b4699308b7b16ae151f2e99db46e1
Thomas Hilaire <thilaire@linagora.com>
2013-09-29 23:19:04 +0200

    Refactoring of js code

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -1,28 +1,63 @@
-var updateUserPanelIdPrefix = "admin-update-user-";
+var admin = {
 
-var fillCurrentUsers = function() {
-    $.get('/admin/getUsers', function(data) {
-        $("#admin-list-user").html(data);
-    });
-}
-var createUser = function(actionUrl, form, userFormId) {
-    createOrUpdateUser(actionUrl, form, userFormId, function() {
-        fillCurrentUsers();
-    });
-}
-var updateUser = function(actionUrl, form, userFormId) {
-    createOrUpdateUser(actionUrl, form, userFormId, function() {});
-    $("#userFormId").dialog( "close" );
-}
-var createOrUpdateUser = function(actionUrl, form, userFormId, finishedHandler) {
-    var serializedForm = form.serialize();
-    var newUserLogin = form.get()[0].login.value;
-    $.post(actionUrl, serializedForm)
-       .done(function (form) {
-           $("#" + userFormId).html(form);
-           finishedHandler();
-       })
-       .fail(function (error) {
-           $("#" + userFormId).html(error.responseText);
-       });
+    updateUserPanelIdPrefix: "admin-update-user-",
+
+    formOf: function(formId) {
+        return $('#' + formId + '-form')
+    },
+
+    fillCurrentUsers: function() {
+        $.get('/admin/getUsers', function(data) {
+            $("#admin-list-user").html(data);
+        });
+    },
+
+    createUserSubmitButton: function(actionUrl, formId) {
+        $('#' + formId + '-submit span').text('Create');
+        admin.formOf(formId).submit(function() {
+            admin.createOrUpdateUser(actionUrl, admin.formOf(formId), formId, function() {
+                admin.fillCurrentUsers();
+            });
+        });
+    },
+
+    updateUserSubmitButton: function(actionUrl, formId) {
+        $('#' + formId + '-submit span').text('Update');
+        admin.formOf(formId).submit(function() {
+            admin.createOrUpdateUser(actionUrl, admin.formOf(formId), formId, function() {
+                admin.fillCurrentUsers();
+                $("#userFormId").dialog( "close" );
+            });
+        });
+    },
+
+    createOrUpdateUser: function(actionUrl, form, formId, finishedHandler) {
+        var serializedForm = form.serialize();
+        var newUserLogin = form.get()[0].login.value;
+        $.post(actionUrl, serializedForm)
+           .done(function (form) {
+               $("#" + formId).html(form);
+               finishedHandler();
+           })
+           .fail(function (error) {
+               $("#" + formId).html(error.responseText);
+           });
+        return false;
+    },
+
+    deleteUser: function(actionUrl, userLogin) {
+        utils.confirmDialog('Delete user ' + userLogin, function (dialogInstance) {
+                $.ajax({
+                    url: actionUrl,
+                    type: 'DELETE'
+                }).done(function() {
+                    admin.fillCurrentUsers();
+                    $(dialogInstance).dialog('close');
+                }).fail(function( msg ) {
+                    var alertContent = 'Error: ' + msg.responseText
+                    alert(alertContent);
+                });
+            }
+       );
+    }
 }
--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,23 +1,26 @@
-function showTooltipById(id) {
-	$("#"+id).tooltip({
-		effect: "fade",
-		position: "bottom center",
-		delay: 50,
-		opacity: 0.7});
-}
+var utils = {
 
-function confirmDialog(message, yesFunction) {
-    $('<div></div>').appendTo('body')
-        .html('<div><h6>Are you sure ?</h6></div>')
-        .dialog({
-            modal: true, title: message, zIndex: 10000, autoOpen: true, width: 'auto', resizable: false,
-            buttons: {
-                Yes: function (event, ui) {
-                    yesFunction(this)
-                },
-                No: function (event, ui) {
-                    $(this).dialog('close');
+    showTooltipById: function(id) {
+        $("#"+id).tooltip({
+            effect: "fade",
+            position: "bottom center",
+            delay: 50,
+            opacity: 0.7});
+    },
+
+    confirmDialog: function(message, yesFunction) {
+        $('<div></div>').appendTo('body')
+            .html('<div><h6>Are you sure ?</h6></div>')
+            .dialog({
+                modal: true, title: message, zIndex: 10000, autoOpen: true, width: 'auto', resizable: false,
+                buttons: {
+                    Yes: function (event, ui) {
+                        yesFunction(this)
+                    },
+                    No: function (event, ui) {
+                        $(this).dialog('close');
+                    }
                 }
-            }
-        });
-};
+            });
+    }
+}
