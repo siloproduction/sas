f7cbb4d9a8af30cdb151158d9e5af1d717fb726e
Thomas Hilaire <thilaire@linagora.com>
2014-06-04 23:54:23 +0200

    angularify user WIP

--- public/javascripts/ang-admin.js
+++ public/javascripts/ang-admin.js
@@ -1,29 +1,11 @@
-angular
-    .module('project', ['ngRoute', 'firebase'])
-    .config(function($routeProvider) {
-      $routeProvider
-        .when('/', {
-          controller:'ListCtrl',
-          templateUrl:'list.html'
-        })
-        .when('/edit/:projectId', {
-          controller:'EditCtrl',
-          templateUrl:'detail.html'
-        })
-        .when('/new', {
-          controller:'CreateCtrl',
-          templateUrl:'detail.html'
-        })
-        .otherwise({
-          redirectTo:'/'
-        });
-    })
+angular.module('project', ['ui.bootstrap']);
 
 function AdminUserCtrl($scope, $http) {
   var EMPTY_USER = {id: 0, login: '', password: '', profile: 'User'};
 
   $scope.users = [];
   $scope.user = EMPTY_USER;
+  $scope.creationIsClosed = true;
 
   $scope.refreshUsers = function() {
     $http.get('/admin/getUsersJson')
