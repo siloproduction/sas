d4b249fb86502c0c51cdda1a324ba4d9eecf62fb
Simon Garnier <simongarnier07@hotmail.fr>
2014-09-22 23:35:10 +0200

    add one line in json writers

--- app/json/JsonWriters.scala
+++ app/json/JsonWriters.scala
@@ -7,22 +7,24 @@ import scala.collection.Traversable
 import scala.Traversable
 import play.api.libs.json.Json._
 import play.api.libs.json.JsArray
+import anorm.Pk
 import models.Event
 import play.api.libs.json.JsSuccess
 import play.api.libs.json.JsString
 import scala.Some
 import play.api.libs.json.JsNumber
-/*
-object JsonWriters {
-  implicit val eventReads: Reads[Event] = (
-    (__ \ "id").read[Long] ~
-      (__ \ "name").read[String] ~
-    )(Event.apply _)
 
-  implicit val eventWrites = new Writes[Event] {
-    def writes(event: Event) = Json.obj(
-      "id" -> event.id,
-      "name" -> event.name
-    )
-  }
-}*/
+  /*
+  object JsonWriters {
+    implicit val eventReads: Reads[Event] = (
+      (__ \ "id").read[Pk[Long]] ~
+        (__ \ "name").read[String] ~
+        (__ \ "userId").read[Pk[Long]] ~
+      )(Event.apply _)
+
+    implicit val eventWrites = new Writes[Event] {
+      def writes(event: Event) = Json.obj(
+        "id" -> event.id,
+        "name" -> event.name
+      )
+    }*/
