bb6ca0136ce14f008580d583ede1e62caa498478
Simon Garnier <simongarnier07@hotmail.fr>
2014-09-22 05:46:36 +0200

    delete files for real

--- app/controllers/dao/package.scala
+++
@@ -1,7 +0,0 @@
-package controllers
-
-package object dao {
-
-  case class DAOException(message: String) extends  Exception(message)
-
-}
--- app/controllers/package.scala
+++
@@ -1,9 +0,0 @@
-/**
- * @Author( " b l t C r e w " )
- */
-package object controllers {
-
-  case class UserNotFoundException(msg: String) extends Exception
-  case class InvalidCredentialsException(msg: String) extends Exception
-
-}
